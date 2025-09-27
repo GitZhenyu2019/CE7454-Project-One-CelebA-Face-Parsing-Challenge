9ca203d3a4518b54cfbc4a332eab6d79353b69a0
Zhenyu Yao <15255137027@163.com>
2025-09-27 13:48:12 +0800

    Update trainer.py

--- solution/trainer.py
+++ solution/trainer.py
@@ -59,7 +59,7 @@ class Trainer:
                 labels = labels.to(self.device, non_blocking=True)      # [B, H, W]
 
                 logits = self.model(imgs)                               # [B, 19, H, W]
-                loss, ce, dl = combined_loss(logits=logits, targets=labels, ce_weight=0.7)
+                loss, ce, dl = combined_loss(logits=logits, targets=labels, ce_weight=0.6)
 
                 self.optimizer.zero_grad(set_to_none=True)
                 loss.backward()
@@ -96,4 +96,5 @@ class Trainer:
         
 
         
+
 
